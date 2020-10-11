1f125047ce25a99b0e80a78d1dda9dff31ed515a
Stela Gaši <sg50985@fer.hr>
2020-10-11 18:36:18 +0200

    implemented needed Matrix constructors

--- zadaca1/src/main/java/hr/fer/apr/lu/Matrix.java
+++ zadaca1/src/main/java/hr/fer/apr/lu/Matrix.java
@@ -2,42 +2,42 @@ package hr.fer.apr.lu;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 //todo metoda za mijenjanje dimenzije matrice?
 public class Matrix {
     int numberOfRows, numberOfColumns;
     double[] elements;
 
-    //todo A = B ?
-    public Matrix(Matrix matrix){
-        this(matrix.getNumberOfRows(), matrix.getNumberOfColumns(), matrix.getElements());
+    public Matrix(Matrix matrix) {
+        this.numberOfRows = matrix.getNumberOfRows();
+        this.numberOfColumns = matrix.getNumberOfColumns();
+        this.elements = matrix.getElements();
     }
 
-    public Matrix(File file){
+    public Matrix(String file) {
+        int numberOfColumns = -1;
+        int numberOfRows = 0;
         ArrayList<String> strings = new ArrayList<>();
-        try{
+        try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String line;
-            while((line = bufferedReader.readLine()) !=  null){
-                strings.addAll(line.split());
+            while ((line = bufferedReader.readLine()) != null) {
+                if (numberOfColumns == -1) numberOfColumns = line.split("\\s+").length;
+                else if (numberOfColumns != line.split("\\s+").length) throw new IllegalArgumentException();
+                numberOfRows++;
+                Collections.addAll(strings, line.split("\\s+"));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public Matrix(int numberOfRows, int numberOfColumns, double[] elements) {
-        this.numberOfRows = numberOfRows;
-        this.numberOfColumns = numberOfColumns;
-        this.elements = new double[numberOfRows * numberOfColumns];
-        for (int i = 0; i < numberOfColumns; i++) {
-            for (int j = 0; j < numberOfColumns; j++) {
-                if (i * numberOfColumns + j >= elements.length) break;
-                this.elements[i * numberOfColumns + j] = elements[i * numberOfColumns + j];
-            }
+        double[] elements = new double[strings.size()];
+        for (int i = 0; i < strings.size(); i++) {
+            elements[i] = Double.parseDouble(strings.get(i));
         }
+        this.numberOfColumns = numberOfColumns;
+        this.numberOfRows = numberOfRows;
+        this.elements = elements;
     }
 
     public int getNumberOfRows() {
@@ -55,5 +55,19 @@ public class Matrix {
     public void setElementTo(int numberOfRow, int numberOfColumn, double value) {
         if (numberOfRow >= numberOfRows || numberOfColumn >= numberOfColumns) throw new IllegalArgumentException();
         elements[numberOfRow * numberOfColumns + numberOfColumn] = value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("[");
+        for (int i = 0; i < numberOfRows; i++) {
+            stringBuilder.append("[");
+            for (int j = 0; j < numberOfColumns - 1; j++) {
+                stringBuilder.append(elements[i * numberOfColumns + j]).append(" ");
+            }
+            stringBuilder.append(elements[i * numberOfColumns + numberOfColumns - 1]).append("]\n");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf("\n")).append("]");
+        return stringBuilder.toString();
     }
 }
