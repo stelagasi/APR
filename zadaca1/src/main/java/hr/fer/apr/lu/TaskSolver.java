a06127e50a369f610df6e24bbfff534736af1354
Stela Gaši <sg50985@fer.hr>
2020-10-13 13:35:27 +0200

    added inverse and determinant of matrix

--- zadaca1/src/main/java/hr/fer/apr/lu/TaskSolver.java
+++ zadaca1/src/main/java/hr/fer/apr/lu/TaskSolver.java
@@ -3,13 +3,13 @@ package hr.fer.apr.lu;
 import hr.fer.apr.lu.matrix.SquareMatrix;
 import hr.fer.apr.lu.matrix.Vector;
 
-public class EquationSolver {
+public class TaskSolver {
 
-    private EquationSolver() {
+    private TaskSolver() {
     }
 
     public static Vector solveWithLU(SquareMatrix A, Vector b) {
-        A = LUDecomposition(A);
+        LUDecomposition(A);
         Vector y = forwardSubstitution(A.getL(), b);
         Vector x = backSubstitution(A.getU(), y);
         return x;
@@ -20,6 +20,53 @@ public class EquationSolver {
         Vector y = forwardSubstitution(A.getL(), b);
         Vector x = backSubstitution(A.getU(), y);
         return x;
+    }
+
+    public static SquareMatrix inverseOfMatrix(SquareMatrix matrix) {
+        double[] elements = new double[matrix.getNumberOfRows()];
+        for (int i = 0; i < matrix.getNumberOfRows(); i++) {
+            elements[i] = i;
+        }
+        Vector P = new Vector(matrix.getNumberOfRows(), elements);
+        LUPDecomposition(matrix, P);
+        SquareMatrix result = new SquareMatrix(matrix.getNumberOfRows(), new double[matrix.getNumberOfRows() * matrix.getNumberOfRows()]);
+        for (int i = 0; i < matrix.getNumberOfRows(); i++) {
+            double[] e = new double[matrix.getNumberOfRows()];
+            for (int j = 0; j < e.length; j++) {
+                if (P.getElementAt(j) == i) {
+                    e[j] = 1;
+                    break;
+                }
+            }
+            Vector y = forwardSubstitution(matrix.getL(), new Vector(e.length, e));
+            Vector x = backSubstitution(matrix.getU(), y);
+            for (int j = 0; j < x.getNumberOfRows(); j++) {
+                result.setElementAt(j, i, x.getElementAt(j));
+            }
+        }
+        return result;
+    }
+
+    public static double determinantOfMatrix(SquareMatrix matrix) {
+        double[] elements = new double[matrix.getNumberOfRows()];
+        for (int i = 0; i < matrix.getNumberOfRows(); i++) {
+            elements[i] = i;
+        }
+        Vector P = new Vector(matrix.getNumberOfRows(), elements);
+        LUPDecomposition(matrix, P);
+        int numberOfPermutations = 0;
+        for (int i = 0; i < P.getNumberOfRows(); i++) {
+            if (P.getElementAt(i) != i) numberOfPermutations++;
+        }
+        double l = 1;
+        double u = 1;
+        for (int i = 0; i < matrix.getNumberOfRows(); i++) {
+            l *= matrix.getL().getElementAt(i, i);
+            u *= matrix.getU().getElementAt(i, i);
+        }
+        double determinant = numberOfPermutations % 2 == 1 ? 1 : -1;
+        determinant *= l * u;
+        return determinant;
     }
 
     private static Vector forwardSubstitution(SquareMatrix A, Vector b) {
@@ -41,15 +88,14 @@ public class EquationSolver {
             for (int j = x.getNumberOfRows() - 1; j > i; j--) {
                 x.setElementAt(i, x.getElementAt(i) - A.getElementAt(i, j) * x.getElementAt(j));
             }
-            if (Math.abs(A.getElementAt(i, i)) < 1e-6) throw new ArithmeticException("Division by zero.");
+            if (Math.abs(A.getElementAt(i, i)) < 1e-6)
+                throw new ArithmeticException(String.format("Division by %.20f", A.getElementAt(i, i)));
             x.setElementAt(i, x.getElementAt(i) / A.getElementAt(i, i));
         }
         return x;
     }
 
-    //todo jel hocu resultMatrix ili samo A
-    private static SquareMatrix LUDecomposition(SquareMatrix A) {
-        SquareMatrix resultMatrix = new SquareMatrix(A);
+    private static void LUDecomposition(SquareMatrix A) {
         for (int i = 0; i < A.getNumberOfRows() - 1; i++) {
             for (int j = i + 1; j < A.getNumberOfRows(); j++) {
                 if (Math.abs(A.getElementAt(i, i)) < 1e-6) throw new ArithmeticException("Division by zero.");
@@ -59,10 +105,9 @@ public class EquationSolver {
                 }
             }
         }
-        return resultMatrix;
     }
 
-    private static SquareMatrix LUPDecomposition(SquareMatrix A, Vector b) {
+    private static void LUPDecomposition(SquareMatrix A, Vector b) {
         for (int i = 0; i < A.getNumberOfRows() - 1; i++) {
             int pivot = i;
             for (int j = i + 1; j < A.getNumberOfRows(); j++) {
@@ -78,6 +123,5 @@ public class EquationSolver {
                 }
             }
         }
-        return A;
     }
 }
