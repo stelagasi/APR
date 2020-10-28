f099754fb1d0aabf6e6b96ac9a02f0157c744236
Stela Gaši <sg50985@fer.hr>
2020-10-28 23:20:39 +0100

    added ValueSaver

---
+++ zadaca1/src/main/java/hr/fer/apr/lu/ValueSaver.java
@@ -0,0 +1,29 @@
+package hr.fer.apr.lu;
+
+import hr.fer.apr.lu.matrix.Vector;
+
+public class ValueSaver {
+    private Vector point;
+    private Double value;
+
+    public ValueSaver(Vector point, Double value) {
+        this.point = point;
+        this.value = value;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public Vector getPoint() {
+        return point;
+    }
+
+    public void setPoint(Vector point) {
+        this.point = point;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
