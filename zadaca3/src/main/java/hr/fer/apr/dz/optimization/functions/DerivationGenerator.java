a5103cfd8b1ef026b2b6108188d8f122f5918d16
Stela Gaši <sg50985@fer.hr>
2020-11-29 20:35:41 +0100

    fixed derivations

--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/functions/DerivationGenerator.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/functions/DerivationGenerator.java
@@ -9,40 +9,36 @@ public class DerivationGenerator {
 
     private DerivationGenerator(){}
 
-    public static Function<Vector, Double> f1(int numberOfVariable){
-        if(numberOfVariable > 1 || numberOfVariable < 0) {
-            throw new IllegalArgumentException("Function has only two variables.");
-        }
-        if(numberOfVariable == 0){
-            return vector -> -400 * vector.getElementAt(0) * vector.getElementAt(1)
+    public static Function<Vector, Vector> f1() {
+        return vector -> {
+            double x1 = -400 * vector.getElementAt(0) * vector.getElementAt(1)
                     + 400 * pow(vector.getElementAt(0), 3) + 2 * vector.getElementAt(0) - 2;
-        } else return vector -> 200 * (vector.getElementAt(1) - pow(vector.getElementAt(0), 2));
+            double x2 = 200 * (vector.getElementAt(1) - pow(vector.getElementAt(0), 2));
+            return new Vector(2, new double[]{x1, x2});
+        };
     }
 
-    public static Function<Vector, Double> f2(int numberOfVariable){
-        if(numberOfVariable > 1 || numberOfVariable < 0) {
-            throw new IllegalArgumentException("Function has only two variables.");
-        }
-        if(numberOfVariable == 0){
-            return vector -> 2 * (vector.getElementAt(0) - 4);
-        } else return vector -> 8 * (vector.getElementAt(1) - 2);
+    public static Function<Vector, Vector> f2(){
+        return vector -> {
+            double x1 = 2 * (vector.getElementAt(0) - 4);
+            double x2 = 8 * (vector.getElementAt(1) - 2);
+            return new Vector(2, new double[]{x1, x2});
+        };
     }
 
-    public static Function<Vector, Double> f3(int numberOfVariable){
-        if(numberOfVariable > 1 || numberOfVariable < 0) {
-            throw new IllegalArgumentException("Function has only two variables.");
-        }
-        if(numberOfVariable == 0){
-            return vector -> 2 * (vector.getElementAt(0) - 2);
-        } else return vector -> 2 * (vector.getElementAt(1) + 3);
+    public static Function<Vector, Vector> f3(){
+        return vector -> {
+            double x1 = 2 * (vector.getElementAt(0) - 2);
+            double x2 = 2 * (vector.getElementAt(1) + 3);
+            return new Vector(2, new double[]{x1, x2});
+        };
     }
 
-    public static Function<Vector, Double> f4(int numberOfVariable){
-        if(numberOfVariable > 1 || numberOfVariable < 0) {
-            throw new IllegalArgumentException("Function has only two variables.");
-        }
-        if(numberOfVariable == 0){
-            return vector -> 2 * (vector.getElementAt(0) - 3);
-        } else return vector -> 2 * vector.getElementAt(1);
+    public static Function<Vector, Vector> f4(){
+        return vector -> {
+            double x1 = 2 * (vector.getElementAt(0) - 3);
+            double x2 = 2 * vector.getElementAt(1);
+            return new Vector(2, new double[]{x1, x2});
+        };
     }
 }
