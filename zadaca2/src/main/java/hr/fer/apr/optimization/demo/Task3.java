b672672a145e7e73389278ab4271be2fc79fbcd2
Stela Gaši <sg50985@fer.hr>
2020-11-29 18:31:52 +0100

    small fix

--- zadaca2/src/main/java/hr/fer/apr/optimization/demo/Task3.java
+++ zadaca2/src/main/java/hr/fer/apr/optimization/demo/Task3.java
@@ -12,9 +12,11 @@ public class Task3 {
         Vector dx = new Vector(3, new double[]{0.5, 0.5});
         Vector startingPoint = new Vector(2, new double[]{5, 5});
 
+        System.out.println("Nelder-Mead:");
         System.out.print(NelderMeadSimplex.of(function1, startingPoint, 1, 1, 0.5, 2, 1e-6, 0.5));
         System.out.printf("Broj evaluacija funkcije: %d\n\n", function1.getNumberOfEvaluations());
         System.out.println("----------------------------------------------------------");
+        System.out.println("Hooke-Jeeves:");
         System.out.print(HookeJeevesSearch.of(function2, startingPoint, dx, e));
         System.out.printf("Broj evaluacija funkcije: %d\n\n", function2.getNumberOfEvaluations());
     }
