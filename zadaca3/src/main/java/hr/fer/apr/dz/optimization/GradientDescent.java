c77230fca62874775b2e2cf7b38cfa09bea21081
Stela Gaši <sg50985@fer.hr>
2020-11-30 16:30:26 +0100

    fixed bug with unimodal interval in direction of vector

--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/GradientDescent.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/GradientDescent.java
@@ -1,5 +1,6 @@
 package hr.fer.apr.dz.optimization;
 
+import hr.fer.apr.dz.optimization.functions.FunctionGenerator;
 import hr.fer.apr.lu.matrix.Vector;
 import hr.fer.apr.optimization.GoldenSectionSearch;
 
@@ -19,9 +20,7 @@ public class GradientDescent {
             System.out.println(v);
             if(v.getEuclideanNorm() < epsilon) return nextPoint;
 
-            for (int i = 0; i < point.getNumberOfRows(); i++) {
-                nextPoint.setElementAt(i, point.getElementAt(i) + v.getElementAt(i));
-            }
+            nextPoint = Vector.addition(point, v);
         }
     }
 
@@ -34,20 +33,9 @@ public class GradientDescent {
 
             if (v.getEuclideanNorm() < epsilon) return nextPoint;
 
-            Function<Vector, Double> lambdaFunction = getLambdaFunction(function, v, point);
-            nextPoint = GoldenSectionSearch.of(lambdaFunction, 0, point,
-                    new Vector(1, new double[]{epsilon}), 1);
+            Function<Vector, Double> lambdaFunction = FunctionGenerator.getLambdaFunction(function, v, point);
+            Vector shift = GoldenSectionSearch.ofDirection(lambdaFunction, v, point, epsilon, 1);
+            nextPoint = Vector.addition(point, shift);
         }
-    }
-
-    private static Function<Vector, Double> getLambdaFunction(Function<Vector, Double> function, Vector v, Vector point) {
-        return lambda -> {
-            Vector lambdaVector = new Vector(2, new double[2]);
-            lambdaVector.setElementAt(0, point.getElementAt(0) +
-                    v.getElementAt(0) * lambda.getElementAt(0));
-            lambdaVector.setElementAt(1, point.getElementAt(1) +
-                    v.getElementAt(1) * lambda.getElementAt(0));
-            return function.apply(lambdaVector);
-        };
     }
 }
--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/NewtonRaphsonMethod.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/NewtonRaphsonMethod.java
@@ -1,8 +1,10 @@
 package hr.fer.apr.dz.optimization;
 
+import hr.fer.apr.dz.optimization.functions.FunctionGenerator;
 import hr.fer.apr.lu.matrix.Matrix;
 import hr.fer.apr.lu.matrix.SquareMatrix;
 import hr.fer.apr.lu.matrix.Vector;
+import hr.fer.apr.optimization.GoldenSectionSearch;
 
 import java.util.function.Function;
 
@@ -12,15 +14,31 @@ public class NewtonRaphsonMethod {
 
     private NewtonRaphsonMethod() {}
 
-    public static Vector of(Function<Vector, Double> function, Function<Vector, Vector> gradient, Function<Vector, SquareMatrix> hesseMatrix, Vector startingPoint, double epsilon) {
+    public static Vector of(Function<Vector, Vector> gradient, Function<Vector, SquareMatrix> hesseMatrix, Vector startingPoint, double epsilon) {
         Vector nextPoint = startingPoint;
         while (true) {
             Vector point = nextPoint;
             SquareMatrix H = hesseMatrix.apply(point);
             Vector result = new Vector(Matrix.matrixMultiplication(inverseOfMatrix(H), gradient.apply(point)));
-            nextPoint.setElementAt(0, point.getElementAt(0) - result.getElementAt(0));
-            nextPoint.setElementAt(1, point.getElementAt(1) - result.getElementAt(1));
             if (result.getEuclideanNorm() < epsilon) return point;
+            nextPoint = Vector.subtraction(point, result);
+        }
+    }
+
+    public static Vector withGoldenSection(Function<Vector, Double> function, Function<Vector, Vector> gradient, Function<Vector, SquareMatrix> hesseMatrix, Vector startingPoint, double epsilon){
+        Vector nextPoint = startingPoint;
+
+        while(true){
+            Vector point = nextPoint;
+            SquareMatrix H = hesseMatrix.apply(point);
+            Vector result = new Vector(Matrix.matrixMultiplication(inverseOfMatrix(H), gradient.apply(point)));
+            result = result.multiplicationWithScalar(-1);
+
+            if(result.getEuclideanNorm() < epsilon) return nextPoint;
+
+            Function<Vector, Double> lambdaFunction = FunctionGenerator.getLambdaFunction(function, result, point);
+            Vector shift = GoldenSectionSearch.ofDirection(lambdaFunction, result, point, epsilon, 1);
+            nextPoint = Vector.addition(point, shift);
         }
     }
 }
