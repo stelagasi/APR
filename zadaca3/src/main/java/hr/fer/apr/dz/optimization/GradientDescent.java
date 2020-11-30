38b4ad1e1cbbf3f01535d284b7b03379231d64ab
Stela Gaši <sg50985@fer.hr>
2020-11-30 23:38:09 +0100

    added number of iterations in GradientDescent

--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/GradientDescent.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/GradientDescent.java
@@ -13,29 +13,27 @@ public class GradientDescent {
 
     public static Vector of(Function<Vector, Double> function, Function<Vector, Vector> functionGradient, Vector startingPoint, double epsilon) {
         Vector nextPoint = new Vector(startingPoint);
-        while (true) {
+        for (int i = 0; i < 100; i++) {
             Vector point = nextPoint;
             Vector v = functionGradient.apply(point);
             v = v.multiplicationWithScalar(-1);
-            System.out.println(v);
             if(v.getEuclideanNorm() < epsilon) return nextPoint;
 
             nextPoint = Vector.addition(point, v);
         }
+        return nextPoint;
     }
 
     public static Vector withGoldenSection(Function<Vector, Double> function, Function<Vector, Vector> functionGradient, Vector startingPoint, double epsilon) {
         Vector nextPoint = new Vector(startingPoint);
-        while (true) {
+        for (int i = 0; i < 10000; i++) {
             Vector point = nextPoint;
             Vector v = functionGradient.apply(point);
-            v = v.multiplicationWithScalar(-1);
 
             if (v.getEuclideanNorm() < epsilon) return nextPoint;
 
-            Function<Vector, Double> lambdaFunction = FunctionGenerator.getLambdaFunction(function, v, point);
-            Vector shift = GoldenSectionSearch.ofDirection(lambdaFunction, v, point, epsilon, 1);
-            nextPoint = Vector.addition(point, shift);
+            nextPoint = GoldenSectionSearch.ofDirection(function, v, point, epsilon, 1);
         }
+        return nextPoint;
     }
 }
