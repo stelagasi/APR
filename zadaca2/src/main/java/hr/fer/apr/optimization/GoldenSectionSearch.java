5ceb542ac1e457f1ce8341be9ba1499db6b62a61
Stela Gaši <sg50985@fer.hr>
2020-11-30 16:29:27 +0100

    added golden section search with unimodal interval in direction of vector

--- zadaca2/src/main/java/hr/fer/apr/optimization/GoldenSectionSearch.java
+++ zadaca2/src/main/java/hr/fer/apr/optimization/GoldenSectionSearch.java
@@ -43,11 +43,54 @@ public class GoldenSectionSearch {
         return result;
     }
 
+    public static Vector ofDirection(Function<Vector, Double> function, Vector a, Vector b, double e){
+        Vector c = Vector.subtraction(b, Vector.subtraction(b, a).multiplicationWithScalar(k));
+        Vector d = Vector.addition(a, Vector.subtraction(b, a).multiplicationWithScalar(k));
+
+        double fc = function.apply(c);
+        double fd = function.apply(d);
+
+        while(Vector.subtraction(b, a).getEuclideanNorm() > e){
+            if(fc < fd){
+                b = d;
+                d = c;
+                c = Vector.subtraction(b, Vector.subtraction(b, a).multiplicationWithScalar(k));
+                fd = fc;
+                fc = function.apply(c);
+            } else {
+                a = c;
+                c = d;
+                d = Vector.addition(a, Vector.subtraction(b, a).multiplicationWithScalar(k));
+                fc = fd;
+                fd = function.apply(d);
+            }
+        }
+        return Vector.addition(a, b).multiplicationWithScalar(1./2);
+    }
+
+//    private static boolean compare(Vector v1, Vector v2){
+//        if(v1.getNumberOfRows() != v2.getNumberOfRows()) throw new IllegalArgumentException("Vectors are not the same size");
+//
+//        for(int i = 0; i < v1.getNumberOfRows(); i++){
+//            if(v2.getElementAt(i) >= v1.getElementAt(i)) return false;
+//        }
+//
+//        return true;
+//    }
+
     public static Vector of(Function<Vector, Double> function, int dimension, Vector startingPoint, Vector e, double h){
         ArrayList<Vector> unimodalInterval = UnimodalIntervalSearch.of(function, h, startingPoint, dimension);
         Vector a = unimodalInterval.get(0);
         Vector b = unimodalInterval.get(1);
 
         return of(function, dimension, a, b, e);
+    }
+
+    public static Vector ofDirection(Function<Vector, Double> function, Vector direction, Vector startingPoint, double e, double h){
+        ArrayList<Vector> unimodalInterval = UnimodalIntervalSearch.ofDirection(function, h, startingPoint, direction);
+        Vector a = unimodalInterval.get(0);
+        Vector b = unimodalInterval.get(1);
+
+        return ofDirection(function, a, b, e);
     }
 }
