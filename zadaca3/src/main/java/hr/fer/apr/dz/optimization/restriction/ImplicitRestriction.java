ca842934492ace99ebb05fd667e0c4bef8f7789c
Stela Gaši <sg50985@fer.hr>
2020-12-01 00:02:37 +0100

    changed function type in ImplicitRestriction

--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/restriction/ImplicitRestriction.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/restriction/ImplicitRestriction.java
@@ -5,13 +5,13 @@ import hr.fer.apr.lu.matrix.Vector;
 import java.util.function.Function;
 
 public class ImplicitRestriction extends Restriction {
-    private Function<Vector, Boolean> condition;
+    private Function<Vector, Double> condition;
 
-    public ImplicitRestriction(Function<Vector, Boolean> condition){
+    public ImplicitRestriction(Function<Vector, Double> condition){
         this.condition = condition;
     }
 
-    public Function<Vector, Boolean> getCondition() {
+    public Function<Vector, Double> getCondition() {
         return condition;
     }
 }
