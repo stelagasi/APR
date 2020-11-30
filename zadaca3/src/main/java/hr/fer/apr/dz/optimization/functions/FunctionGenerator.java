64dea22566429a2cc41a99fb88e93f21c932eedf
Stela Gaši <sg50985@fer.hr>
2020-11-30 16:29:54 +0100

    added function that has only lambda as variable

--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/functions/FunctionGenerator.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/functions/FunctionGenerator.java
@@ -27,4 +27,15 @@ public class FunctionGenerator {
     public static Function<Vector, Double> f4(){
         return vector -> pow(vector.getElementAt(0) - 3, 2) + pow(vector.getElementAt(1), 2);
     }
+
+    public static Function<Vector, Double> getLambdaFunction(Function<Vector, Double> function, Vector v, Vector point) {
+        return lambda -> {
+            Vector lambdaVector = new Vector(2, new double[2]);
+            lambdaVector.setElementAt(0, point.getElementAt(0) +
+                    v.getElementAt(0) * lambda.getElementAt(0));
+            lambdaVector.setElementAt(1, point.getElementAt(1) +
+                    v.getElementAt(1) * lambda.getElementAt(0));
+            return function.apply(lambdaVector);
+        };
+    }
 }
