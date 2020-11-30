a3806ba54c0c72cd09453a6634fb937dd8f4c720
Stela Gaši <sg50985@fer.hr>
2020-11-30 23:38:43 +0100

    removed getLambdaFunction

--- zadaca3/src/main/java/hr/fer/apr/dz/optimization/functions/FunctionGenerator.java
+++ zadaca3/src/main/java/hr/fer/apr/dz/optimization/functions/FunctionGenerator.java
@@ -11,7 +11,7 @@ public class FunctionGenerator {
     }
 
     public static Function<Vector, Double> f1() {
-        return vector -> 100 * pow((vector.getElementAt(1) - pow(vector.getElementAt(0), 2)), 2)
+        return vector -> 100 * pow(vector.getElementAt(1) - pow(vector.getElementAt(0), 2), 2)
                 + pow(1 - vector.getElementAt(0), 2);
     }
 
@@ -26,16 +26,5 @@ public class FunctionGenerator {
 
     public static Function<Vector, Double> f4(){
         return vector -> pow(vector.getElementAt(0) - 3, 2) + pow(vector.getElementAt(1), 2);
-    }
-
-    public static Function<Vector, Double> getLambdaFunction(Function<Vector, Double> function, Vector v, Vector point) {
-        return lambda -> {
-            Vector lambdaVector = new Vector(2, new double[2]);
-            lambdaVector.setElementAt(0, point.getElementAt(0) +
-                    v.getElementAt(0) * lambda.getElementAt(0));
-            lambdaVector.setElementAt(1, point.getElementAt(1) +
-                    v.getElementAt(1) * lambda.getElementAt(0));
-            return function.apply(lambdaVector);
-        };
     }
 }
