449fdbdf2d04019f93836bc281e7342f8ea769db
stelagasi <stela.gasi8@gmail.com>
2021-01-03 01:53:09 +0100

    added method interfaces

---
+++ zadaca5/src/main/java/hr/fer/apr/numericintegration/ExplicitMethod.java
@@ -0,0 +1,9 @@
+package hr.fer.apr.numericintegration;
+
+import hr.fer.apr.lu.matrix.Matrix;
+import hr.fer.apr.lu.matrix.Vector;
+
+public interface ExplicitMethod {
+
+    Vector apply(Matrix A, Vector x0, Matrix B, String[] rt, double T, double tMax);
+}
---
+++ zadaca5/src/main/java/hr/fer/apr/numericintegration/ImplicitMethod.java
@@ -0,0 +1,12 @@
+package hr.fer.apr.numericintegration;
+
+import hr.fer.apr.lu.matrix.Matrix;
+import hr.fer.apr.lu.matrix.Vector;
+
+public interface ImplicitMethod {
+
+    void transformToExplicit(Matrix A, Matrix B, double T);
+
+    Vector apply(Vector x0, String[] rt, double T, double tMax);
+
+}
