268c2ebaf0841e5dcb71235feb4b7dea0f4c3c24
stelagasi <stela.gasi8@gmail.com>
2021-01-09 20:50:38 +0100

    fixed bug

--- zadaca5/src/main/java/hr/fer/apr/numericintegration/PredictorCorrectorMethod.java
+++ zadaca5/src/main/java/hr/fer/apr/numericintegration/PredictorCorrectorMethod.java
@@ -17,27 +17,29 @@ public class PredictorCorrectorMethod {
     }
 
     public Vector apply(Matrix A, Vector x0, Matrix B, String[] rt, double T, double tMax, int numberOfCorrectorApplies, int numberOfPrintingIteration, boolean calculateError){
-        Vector x = new Vector(x0);
+        Vector xCurrent = new Vector(x0);
+        Vector xNext = new Vector(x0);
         int numberOfIteration = 0;
         x1 = new double[(int) (tMax/T)];
         x2 = new double[(int) (tMax/T)];
 
         for(double i = T; i <= tMax; i = i + T){
-            Vector xApprox = predictor.apply(A, x, B, rt, T, i, i, 0, false);
+            Vector xApprox = predictor.apply(A, xCurrent, B, rt, T, i, i, 0, false);
 
             for (int j = 0; j < numberOfCorrectorApplies; j++) {
-                x = corrector.correct(A, B, x, xApprox, rt, T, i);
-                xApprox = x;
+                xNext = corrector.correct(A, B, xCurrent, xApprox, rt, T, i);
+                xApprox = xNext;
             }
-            if(calculateError) methodHelper.calculateError(x, i);
+            xCurrent = xNext;
+            if(calculateError) methodHelper.calculateError(xCurrent, i);
             if (numberOfIteration++ % numberOfPrintingIteration == 0) {
-                methodHelper.getStringBuilder().append(x).append(System.lineSeparator());
-                x1[numberOfIteration - 1] = x.getElementAt(0);
-                x2[numberOfIteration - 1] = x.getElementAt(1);
+                methodHelper.getStringBuilder().append(xCurrent).append(System.lineSeparator());
+                x1[numberOfIteration - 1] = xCurrent.getElementAt(0);
+                x2[numberOfIteration - 1] = xCurrent.getElementAt(1);
             }
         }
 
-        return x;
+        return xCurrent;
     }
 
     public MethodHelper getMethodHelper() {
